ff3c77828beb3274b0efc3f60c2f17fe8f3aca75
kunal <kunal.py3@gmail.com>
2022-04-12 16:29:14 +0530

    Day 2

--- src/components/mdRenderer.jsx
+++ src/components/mdRenderer.jsx
@@ -1,27 +1,37 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useRef } from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import axios from 'axios'
 
 export default function mdRenderer() {
-    const [prev, setPrev] = useState(false)
+    const AreaRef = useRef()
+    const MdRef = useRef()
+    const [text, setText] = useState('')
 
-    return (
-        <div className="card text-center">
-            <div className="card-header">
-                <ul className="nav nav-tabs card-header-tabs">
-                    <li className="nav-item">
-                        <Link className={`nav-link ${!prev ? "" : "active"}`} aria-current="true" to="#">Text</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className={`nav-link ${prev ? "" : "active"}`} to="#">Preview</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="card-body">
-                <div className="form-floating">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "100px" }}></textarea>
-                    <label for="floatingTextarea2">Enter Markdown</label>
-                </div>
-            </div>
+    async function _EvHandler() {
+        const text = AreaRef.current.value
+        // console.log(text)
+        await axios.post("https://api.github.com/markdown", {
+            text, mode: "gfm"
+        })
+            .then((res) => {
+                setText(res.data)
+                MdRef.current.innerHTML = res.data
+            })
+    }
+
+    return (<div className='my-5'>
+        <h3 className='text-secondary'>Text and Markdown  Rendering</h3>
+        <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">Text</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="20" ref={AreaRef}></textarea>
         </div>
-    )
+        <button type="button" className="btn btn-secondary" onClick={_EvHandler}><i className="bi bi-arrow-clockwise"></i></button>
+        <h6 className='my-4'>Markdown</h6>
+        <div className="my-3" ref={MdRef}>
+
+        </div>
+    </div>)
+
 }
+
